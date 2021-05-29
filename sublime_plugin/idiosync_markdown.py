2f9f9186f97751b1b77a38b1b99425a7c3ddc224
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2021-05-29 09:47:05 +0200

    Add files via upload

--- sublime_plugin/idiosync_markdown.py
+++ sublime_plugin/idiosync_markdown.py
@@ -4,13 +4,13 @@
 # moram da skidam nepotrebne informacije koje plugin doda i naravno,
 # da sredim uvlačenje.
 #
-# U člancima koje pišem, u najvećoj meri  koristim samo paragrafe, 
+# U člancima koje pišem, u najvećoj meri  koristim samo paragrafe,
 # naslove (h1, h2, h3, h4) i ulančane liste (sve ostalo lako rešavam
 # preko snipeta), pa se čini da je ovakav, 'idiosinkratični' DIY plugin
 # pravo stvar za takvu situaciju. :)
 #
 # Sintaksa (donekle se poklapa sa zvaničnom specifikacijom):
-# 
+#
 #     #    - Dodaje "<h1>" i "</h1>" oko reda koji počinje sa "#"
 #     ##   - Dodaje "<h2>" i "</h2>" oko reda koji počinje sa "##"
 #     ###  - Dodaje "<h3>" i "</h3>" oko reda koji počinje sa "###
@@ -36,133 +36,135 @@ import sublime_plugin
 import re
 import os
 
+
 class idiosync_markdown(sublime_plugin.TextCommand):
 
-	datoteka_za_upis = ""
+    datoteka_za_upis = ""
 
-	def run(self, edit):
+    def run(self, edit):
 
-		r = sublime.Region(0, self.view.size())
-		s = self.view.substr(r)
-		s = self.idiosyncParse(s)
-		s = self.zamena_tokena_slike(s)
-		
-		#sublime.set_clipboard(s) # - po želji :)
+        r = sublime.Region(0, self.view.size())
+        s = self.view.substr(r)
+        s = self.idiosyncParse(s)
+        s = self.zamena_tokena_slike(s)
 
-		if self.datoteka_za_upis != "":
-			f = open(self.datoteka_za_upis, "wb")
-			s = s.encode("utf-8")
-			f.write(s)
-			f.close()
-		else:
-			ime = self.view.file_name().replace("imd", "html")
+        # sublime.set_clipboard(s)
+        # - po želji :)
 
-			if sublime.active_window().find_open_file(ime):
-				f = open(ime, "wb")
-				s = s.encode("utf-8")
-				f.write(s)
-				f.close()
-			else:
-				n = sublime.active_window().new_file()
-				n.set_name(os.path.basename(ime))
-				n.set_syntax_file("Packages/HTML/HTML.sublime-syntax")
-				n.insert(edit, 0, s)
+        if self.datoteka_za_upis != "":
+            f = open(self.datoteka_za_upis, "wb")
+            s = s.encode("utf-8")
+            f.write(s)
+            f.close()
+        else:
+            ime = self.view.file_name().replace("imd", "html")
 
-	def idiosyncParse(self, s):
-		redovi = s.split("\n")
-		tokeni = []
-		s      = ""
-		parse  = True
+            if sublime.active_window().find_open_file(ime):
+                f = open(ime, "wb")
+                s = s.encode("utf-8")
+                f.write(s)
+                f.close()
+            else:
+                n = sublime.active_window().new_file()
+                n.set_name(os.path.basename(ime))
+                n.set_syntax_file("Packages/HTML/HTML.sublime-syntax")
+                n.insert(edit, 0, s)
 
-		self.datoteka_za_upis = ""
-		
-		for r in redovi:
-			if parse == True:
-				if r == "" or r == "\r":
-					continue
-			
-			if parse == True:
-				if r.startswith("``"):
-					parse = False
-					continue
-				self.ucitavanje_tokena(r, tokeni)
-				
-			else:
-				if r.startswith("~~"):
-					parse = True
-					tokeni.append(["", "", ""])
-					continue
-				tokeni.append([r, "", ""])
+    def idiosyncParse(self, s):
+        redovi = s.split("\n")
+        tokeni = []
+        s      = ""
+        parse  = True
 
-		for t in tokeni:
-			s = s + t[0] + t[1] + t[2] + "\n"
+        self.datoteka_za_upis = ""
 
-		return s
+        for r in redovi:
+            if parse:
+                if r == "" or r == "\r":
+                    continue
 
-	def ucitavanje_tokena(self, r, tokeni):
-		
-		if r.startswith("@@"):
-			self.datoteka_za_upis = r.lstrip('@')
-			return
+            if parse:
+                if r.startswith("``"):
+                    parse = False
+                    continue
+                self.ucitavanje_tokena(r, tokeni)
 
-		if r == "":
-			tokeni.append("", "", "")
-		
-		if r.startswith("!!"):
-			return
-		# Za sada mi ne trebaju h5 i h6, ali, ako zatreba ....
-		# if r.startswith("######"):
-		# 	tokeni.append(["<h6>", r.lstrip('#').strip(), "</h6>\n"])
-		# 	continue
-		# if r.startswith("#####"):
-		# 	tokeni.append(["<h5>", r.lstrip('#').strip(), "</h5>\n"])
-		# 	continue
-		if r.startswith("####"):
-			tokeni.append(["<h4>", r.lstrip('#').strip(), "</h4>\n"])
-			return
-		
-		if r.startswith("###"):
-			tokeni.append(["<h3>", r.lstrip('#').strip(), "</h3>\n"])
-			return
-		
-		if r.startswith("##"):
-			tokeni.append(["<h2>", r.lstrip('#').strip(), "</h2>\n"])
-			return
-		
-		if r.startswith("#"):
-			tokeni.append(["<h1>", r.lstrip('#').strip(), "</h1>\n"])
-			return
-		
-		if r.startswith("**"):
-			tokeni.append(["</ul>\n", "",        ""])
-			return
-		
-		if r.startswith("*"):
-			tokeni.append(["<ul>", "",        ""])
-			return
-		
-		if r.startswith("\t"):
-			tokeni.append(["\t<li>", r.strip(), "</li>"])
-			return
+            else:
+                if r.startswith("~~"):
+                    parse = True
+                    tokeni.append(["", "", ""])
+                    continue
+                tokeni.append([r, "", ""])
 
-		# Ako red nije ništa 'posebno', onda je <p> :)
-		tokeni.append(["<p>\n\t", r.strip(), "\n</p>\n"])
+        for t in tokeni:
+            s = s + t[0] + t[1] + t[2] + "\n"
 
-	# --------------------------------------------------
-	# Automatska numeracija slika u tekstu. Za ovo
-	# koristim i posebne snipete, a sve zajedno to
-	# verovatno ne bi bilo zanimljivo svima, pa se
-	# zato metoda ne poziva po default-u
-	# --------------------------------------------------
+        return s
 
-	def zamena_tokena_slike(self, s):
-	    lista = re.split("#s#", s)
+    def ucitavanje_tokena(self, r, tokeni):
 
-	    n = ""
-	    i = 1
+        if r.startswith("@@"):
+            self.datoteka_za_upis = r.lstrip('@')
+            return
 
-	    for l in lista:
-	    	n = n + l + str(i)
-	    	i = i + 1
+        if r == "":
+            tokeni.append("", "", "")
 
-	    return n.rstrip('0123456789\n ') + "\n"
+        if r.startswith("!!"):
+            return
+        # Za sada mi ne trebaju h5 i h6, ali, ako zatreba ....
+        # if r.startswith("######"):
+        #     tokeni.append(["<h6>", r.lstrip('#').strip(), "</h6>\n"])
+        #     continue
+        # if r.startswith("#####"):
+        #     tokeni.append(["<h5>", r.lstrip('#').strip(), "</h5>\n"])
+        #     continue
+        if r.startswith("####"):
+            tokeni.append(["<h4>", r.lstrip('#').strip(), "</h4>\n"])
+            return
+
+        if r.startswith("###"):
+            tokeni.append(["<h3>", r.lstrip('#').strip(), "</h3>\n"])
+            return
+
+        if r.startswith("##"):
+            tokeni.append(["<h2>", r.lstrip('#').strip(), "</h2>\n"])
+            return
+
+        if r.startswith("#"):
+            tokeni.append(["<h1>", r.lstrip('#').strip(), "</h1>\n"])
+            return
+
+        if r.startswith("**"):
+            tokeni.append(["</ul>\n", "",        ""])
+            return
+
+        if r.startswith("*"):
+            tokeni.append(["<ul>", "",        ""])
+            return
+
+        if r.startswith("\t"):
+            tokeni.append(["\t<li>", r.strip(), "</li>"])
+            return
+
+        # Ako red nije ništa 'posebno', onda je <p> :)
+        tokeni.append(["<p>\n\t", r.strip(), "\n</p>\n"])
+
+    # --------------------------------------------------
+    # Automatska numeracija slika u tekstu. Za ovo
+    # koristim i posebne snipete, a sve zajedno to
+    # verovatno ne bi bilo zanimljivo svima, pa se
+    # zato metoda ne poziva po default-u
+    # --------------------------------------------------
+
+    def zamena_tokena_slike(self, s):
+        lista = re.split("#s#", s)
+
+        n = ""
+        i = 1
+
+        for token in lista:
+            n = n + token + str(i)
+            i = i + 1
+
+        return n.rstrip('0123456789\n ') + "\n"
